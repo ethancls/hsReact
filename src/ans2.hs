90594f63d79dc0f2249c757ba084ac15da1179b2
Dmytro_Palahin <dmytro.palahin@gmail.com>
2024-09-27 13:56:36 +0200

    🔥 all from files

--- src/ans2.hs
+++ src/ans2.hs
@@ -1,19 +1,22 @@
 {-# OPTIONS_GHC -Wno-x-partial #-}
 
+--    *********************** IMPORTS ***********************
 import Control.Monad (when)
 import Data.List (nub)
 
--- Définitions des types
+--    *********************** TYPES ***********************
 type Entites = String
 type Generateur = [Entites]
 type Sequence = [Entites]
 
+--    *********************** DATA ***********************
 -- Définition d'une réaction avec les réactifs, les inhibiteurs et les produits
 data Reaction = Reaction {reactifs :: [Entites], inhibiteurs :: [Entites], produits :: [Entites]} deriving (Show, Eq)
 
 -- Définition d'un arbre N-aire avec suivi de l'historique
 data Arbre a = Feuille a | Noeud a [Arbre a] deriving (Show)
 
+--    *********************** VARIABLES ***********************
 reactionTest1 :: [Reaction]
 reactionTest1 =
     [ Reaction ["a"] ["b"] ["c"] -- Si "a" est présent sans "b", "c" est produit
@@ -33,8 +36,7 @@ reactionTest2 =
     , Reaction ["erk12"] [] ["p70s6k"]
     ]
 
--- ******** FONCTIONS DE VÉRIFICATIONS ********
-
+--    *********************** FONCTIONS DE VÉRIFICATIONS ***********************
 -- Vérifier si une réaction est activée (c'est-à-dire tous les réactifs sont présents et aucun inhibiteur n'est présent)
 verifReac :: Sequence -> Reaction -> Bool
 verifReac sequence reaction =
@@ -85,7 +87,59 @@ genererTousCas generateur reactions profondeur = genererTousCasAux generateur re
             let currentRes = [g : res | res <- currentResTemp, g <- generateur]
             genererTousCasAux generateur reactions profondeur (currentDepth + 1) currentRes
 
--- TESTS
+--    *********************** FONCTIONS DE CHARGEMENT ***********************
+-- Fonction pour découper une chaîne en fonction d'un séparateur
+decouperPar :: Char -> String -> [String]
+decouperPar separateur = foldr (\c l -> if c == separateur then [] : l else (c : head l) : tail l) [[]]
+
+-- Fonction pour parser une ligne de réaction du fichier
+parserReaction :: String -> Reaction
+parserReaction str =
+    let parties = decouperPar ';' str
+        reactifs = decouperPar ',' (head parties)
+        inhibiteurs = decouperPar ',' (parties !! 1)
+        produits = decouperPar ',' (parties !! 2)
+     in Reaction reactifs inhibiteurs produits
+
+-- Fonction pour charger les réactions depuis un fichier
+chargerReactions :: FilePath -> IO [Reaction]
+chargerReactions chemin = do
+    contenu <- readFile chemin
+    return $ map parserReaction (lines contenu)
+
+chargeAfficherReactions :: FilePath -> IO ()
+chargeAfficherReactions chemin = do
+    reactions <- chargerReactions chemin
+    putStrLn "\n                    ------- REACTIONS -------"
+    mapM_ print reactions -- Afficher chaque réaction sur une nouvelle ligne
+    putStrLn "\n"
+
+parserGenerateur :: String -> Generateur
+parserGenerateur = decouperPar ','
+
+chargerGenerateur :: FilePath -> IO Generateur
+chargerGenerateur chemin = do
+    contenu <- readFile chemin
+    return $ parserGenerateur (head (lines contenu))
+
+chargeAfficherGenerateur :: FilePath -> IO ()
+chargeAfficherGenerateur chemin = do
+    generateur <- chargerGenerateur chemin
+    putStrLn "\n                    ------- GENERATEUR -------"
+    print generateur
+    putStrLn "\n"
+
+--    *********************** MAIN ***********************
+main :: IO ()
+main = do
+    chargeAfficherReactions "reactions.txt"
+    chargeAfficherGenerateur "generateur.txt"
+    let profondeur = 5
+    generateur <- chargerGenerateur "generateur.txt"
+    reactions <- chargerReactions "reactions.txt"
+    genererTousCas generateur reactions profondeur
+
+--    *********************** TESTS ***********************
 -- verifReac ["a","c","d"] (Reaction ["a"] ["b"] ["c"]) --> True
 -- verifSequence [["a"],["b"]] reactionTest1 --> [["c"],[]]
 -- verifSequence [["a","c"],["b","c"],["a", ""],["b", ""]] reactionTest1 --> [["c"],["d"],["c"],[]]
@@ -96,3 +150,9 @@ genererTousCas generateur reactions profondeur = genererTousCasAux generateur re
 -- appliquerReactionsUnique ["a","c"] reactionTest1 --> ["c","d"]
 -- appliquerReactionsUnique ["b"] reactionTest1 --> []
 -- genererTousCas generateurTest reactionTest1 profondeur
+-- chargeAfficherReactions "reactions.txt"
+-- chargeAfficherGenerateur "generateur.txt"
+
+--    *********************** CHARGEMENT ***********************
+-- :l ./src/ans2.hs
+-- main
