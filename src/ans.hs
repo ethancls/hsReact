689775bcff19d600337cd3d1aa7212d5943b055a
ethancls <ethancls@icloud.com>
2024-09-27 14:25:03 +0200

    Automatic commit on 2024-09-27 14:25:03

--- src/ans.hs
+++ src/ans.hs
@@ -1,4 +1,27 @@
-import Data.List ( intercalate, nub )
+{-
+
+\**** REACTION SYSTEMS ****
+
+\$ E.NICOLAS 12100466 ethan.bento-nicolas@edu.univ-paris13.fr
+\$ D.PALAHIN 12106973 dmytro.palahin@edu.univ-paris13.fr
+
+Simple project for testing systems under string
+format with sequences of entities or with process
+on the environment.
+
+We can also define a system as a set of logical propositions i.e P = {φ1 = e1 ∧¬e2, ...}
+
+-}
+
+-- ******** IMPORTS ********
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use when" #-}
+{-# HLINT ignore "Avoid lambda using `infix`" #-}
+
+import Control.Monad qualified
+import Data.List (intercalate)
+
+-- ******* DEFINITIONS DES TYPES | STRUCTURES *******
 
 -- Définitions des types
 type Entites = String
@@ -8,76 +31,101 @@ type Sequence = [Entites]
 -- Définition d'une réaction avec les réactifs, les inhibiteurs et les produits
 data Reaction = Reaction {reactifs :: [Entites], inhibiteurs :: [Entites], produits :: [Entites]} deriving (Show, Eq)
 
--- Définition d'un arbre N-aire avec suivi de l'historique
-data Arbre a = Feuille a | Noeud a [Arbre a] deriving (Show)
+reactionTest1 :: [Reaction]
+reactionTest1 =
+    [ Reaction ["a"] ["b"] ["c"] -- Si "a" est présent sans "b", "c" est produit
+    , Reaction ["c"] ["a"] ["d"] -- Si "c" est présent sans "a", "d" est produit
+    ]
+
+generateurTest :: Generateur
+generateurTest = ["a", "b"]
+
+reactionTest2 :: [Reaction]
+reactionTest2 =
+    [ Reaction ["egf"] ["e", "p"] ["erbb1"]
+    , Reaction ["egf"] [] ["erk12"]
+    , Reaction ["erk12"] [] ["p70s6k"]
+    ]
 
 -- ******** FONCTIONS DE VERIFICATIONS ********
 
 -- Vérifier si une réaction est activée (c'est-à-dire tous les réactifs sont présents et aucun inhibiteur n'est présent)
+-- verifReac ["a","c","d"] (Reaction ["a"] ["b"] ["c"])
 verifReac :: Sequence -> Reaction -> Bool
 verifReac sequence reaction =
     all (`elem` sequence) (reactifs reaction) && not (any (`elem` sequence) (inhibiteurs reaction))
 
--- Appliquer les réactions pour produire de nouvelles entités
-applyReactionsUnique :: [Reaction] -> Sequence -> Sequence
-applyReactionsUnique reactions env = nub $ concat [produits r | r <- reactions, verifReac env r]
-
--- ******** PROCESSUS RÉCURSIF AVEC CONSTRUCTION D'UN ARBRE N-AIRE ********
-
--- Fonction pour générer l'arbre avec toutes les possibilités
-processusRecNAire :: Sequence -> [Reaction] -> Generateur -> Int -> [Sequence] -> Arbre (Sequence, Sequence)
-processusRecNAire env reactions [] _ _ = Feuille (env, env)
-processusRecNAire env reactions generateur depth history
-    | depth == 0 = Feuille (env, env)
-    | otherwise =
-        -- Appliquer les réactions à l'environnement actuel
-        let envReagit = applyReactionsUnique reactions env
-            newHistory = env : history
-            -- Créer les sous-arbres seulement si la nouvelle séquence n'a pas déjà été traitée
-            sousArbres = [processusRecNAire (applyReactionsUnique reactions (envReagit ++ [e])) reactions generateur (depth - 1) newHistory
-                            | e <- generateur, (envReagit ++ [e]) `notElem` newHistory]
-        in Noeud (env, envReagit) sousArbres
-
--- ******** AFFICHAGE DU PROCESSUS ********
-
-{- afficheArbre :: Sequence -> Arbre (Sequence, Sequence) -> Generateur -> [Reaction] -> IO ()
-afficheArbre _ (Feuille _) _ _ = return ()  -- Ne rien afficher pour les feuilles
-afficheArbre input (Noeud (env, output) enfants) (context:generateur) reactions = do
-    -- Construire la liste dynamique du processus rec X. (a.X + b.X + c.X + ...)
-    let processusStr = "rec X. (" ++ intercalate " + " (map (++ ".X") (context:generateur)) ++ ")"
-    -- Appliquer les réactions pour produire de nouvelles entités
-    let newOutput = applyReactionsUnique reactions (env ++ [context])
-    -- Afficher la ligne du processus
-    putStrLn $ "Input: " ++ show env ++ " , proc: " ++ processusStr ++ ", context: " ++ context ++ ", output: " ++ show newOutput
-    -- Afficher les sous-arbres enfants avec l'environnement mis à jour
-    case enfants of
-        [] -> return ()  -- Handle the case where enfants is an empty list
-        [enfant@(Noeud _ _)] -> afficheArbre newOutput enfant generateur reactions  -- Handle the case where enfants is a single Noeud
-        [enfant@(Feuille _)] -> afficheArbre newOutput enfant generateur reactions  -- Handle the case where enfants is a single Feuille
-        _  -> mapM_ (\enfant -> afficheArbre newOutput enfant generateur reactions) enfants
-afficheArbre _ (Noeud _ enfants) [] reactions = do
-    -- Afficher les sous-arbres enfants avec l'environnement mis à jour
-    case enfants of
-        [] -> return ()  -- Handle the case where enfants is an empty list
-        [enfant@(Noeud _ _)] -> afficheArbre [] enfant [] reactions  -- Handle the case where enfants is a single Noeud
-        [enfant@(Feuille _)] -> afficheArbre [] enfant [] reactions  -- Handle the case where enfants is a single Feuille
-        _  -> mapM_ (\enfant -> afficheArbre [] enfant [] reactions) enfants
- -}
-
-
--- ******** SYSTÈME DE TEST *********
-
--- Système de réactions (vous pouvez changer les réactions ici)
-alphaSystem :: [Reaction]
-alphaSystem =
-    [ Reaction ["a"] ["b"] ["c"]  -- Si "a" est présent sans "b", "c" est produit
-    , Reaction ["c"] ["a"] ["d"]  -- Si "c" est présent sans "a", "d" est produit
+-- Test des séquences sur l'ensemble des réactions
+-- verifSequence [["a"],["b"]] reactionTest1 --> [["c"],[]]
+-- verifSequence [["a","c"],["b","c"],["a", ""],["b", ""]] reactionTest1 --> [["c"],["d"],["c"],[]]
+verifSequence :: [Sequence] -> [Reaction] -> [[Entites]]
+verifSequence sequences reactions =
+    [ concat [produits reaction | reaction <- reactions, verifReac sequence reaction]
+    | sequence <- sequences
     ]
 
--- ******** EXEMPLE D'UTILISATION *********
+-- verifSequenceEach [["a"],["b"]] reactionTest1 --> [["c"],[]]
+-- verifSequenceEach [["a","c"],["b","c"],["a"],["b"]] reactionTest1 --> [["c","d"],["d"],["c"],[]]
+verifSequenceEach :: [Sequence] -> [Reaction] -> [[Entites]]
+verifSequenceEach sequences reactions =
+    [ applyReactionsUnique sequence reactions
+    | sequence <- sequences
+    ]
+
+-- ******** FONCTIONS DE TRAITEMENT ********
+
+-- procRec ["egf"] reactionTest2 --> [["erbb1","erk12"],["p70s6k"]]
+procRec :: Generateur -> [Reaction] -> [Sequence]
+procRec gLst reactions = processusRec [] gLst reactions
+  where
+    processusRec :: Sequence -> Generateur -> [Reaction] -> [Sequence]
+    processusRec env gLst reactions =
+        let initEnv = env ++ gLst
+         in notreNub (processusRecAux reactions initEnv)
+      where
+        processusRecAux :: [Reaction] -> Sequence -> [Sequence]
+        processusRecAux reactions env =
+            let newEnv = applyReactionsUnique env reactions
+             in if null newEnv
+                    then []
+                    else newEnv : processusRecAux reactions newEnv
+
+-- notreNub (cycle [1,2,3])
+notreNub :: (Eq a) => [a] -> [a]
+notreNub lst = reverse (notreNubAux lst [])
+  where
+    notreNubAux [] acc = acc
+    notreNubAux (x : xs) acc = if x `elem` acc then notreNubAux [] acc else notreNubAux xs (x : acc)
+
+-- Appliquer les réactions pour produire de nouvelles entités
+-- applyReactionsUnique ["a","c"] reactionTest1 --> ["c","d"]
+-- applyReactionsUnique ["b"] reactionTest1 --> []
+applyReactionsUnique :: Sequence -> [Reaction] -> Sequence
+applyReactionsUnique sequence reactions = foldl appliquerReactionsPourEntite [] sequence
+  where
+    appliquerReactionsPourEntite acc entite = foldl ajouterUnique acc (concatMap produitsSiActive reactions) -- Appliquer les réactions pour une entité spécifique et mettre à jour la séquence finale
+      where
+        produitsSiActive r = if verifReac [entite] r then produits r else []
+    ajouterUnique acc x = if x `elem` acc then acc else acc ++ [x] -- Ajouter des éléments uniques à la séquence résultante
 
 
--- ******** FONCTION POUR LIRE LES REACTIONS D'UN FICHIER *********
+-- ******** FONCTIONS REC ********
+
+-- doTree generateurTest reactionTest1 depth
+doTree :: Generateur -> [Reaction] -> Integer -> IO ()
+doTree generateur reactions depth = doTreeAux generateur reactions depth 0 [] [[]]
+   where
+     doTreeAux generateur reactions depth currentDepth currentRes previousRes =
+         if currentDepth < depth
+             then do
+                 print previousRes
+                 let currentResTemp = verifSequenceEach previousRes reactions
+                 let currentRes = concatMap (\res -> map (\g -> g : res) generateur) currentResTemp
+                 doTreeAux generateur reactions depth (currentDepth + 1) [] currentRes
+             else return ()
+
+
+-- ******** LECTURE DES RÉACTIONS À PARTIR D'UN FICHIER *********
 
 -- Fonction pour découper une chaîne en fonction d'un séparateur
 splitBy :: Char -> String -> [String]
@@ -97,3 +145,14 @@ loadReactions :: FilePath -> IO [Reaction]
 loadReactions path = do
     contents <- readFile path
     return $ map parseReaction (lines contents)
+
+
+-- ******** MAIN ********
+
+main :: IO ()
+main = do
+    let depth = 5
+    let generateur = ["a", "b"]
+    reactions <- loadReactions "reactions.txt"
+    doTree generateur reactions depth
+    return ()
