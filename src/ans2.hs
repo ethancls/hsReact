0d21b3937f90ee79beb04066e2f30152bea72b2d
Dmytro_Palahin <dmytro.palahin@gmail.com>
2024-09-27 12:58:03 +0200

    do all in french & optimize the code

--- src/ans2.hs
+++ src/ans2.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -Wno-x-partial #-}
 
-import Control.Monad qualified
-import Data.List (intercalate)
+import Control.Monad (when)
+import Data.List (nub)
 
 -- Définitions des types
 type Entites = String
@@ -23,8 +23,8 @@ reactionTest1 =
 generateurTest :: Generateur
 generateurTest = ["a", "b"]
 
-depth :: Integer
-depth = 5
+profondeur :: Integer
+profondeur = 5
 
 reactionTest2 :: [Reaction]
 reactionTest2 =
@@ -33,85 +33,66 @@ reactionTest2 =
     , Reaction ["erk12"] [] ["p70s6k"]
     ]
 
--- ******** FONCTIONS DE VERIFICATIONS ********
+-- ******** FONCTIONS DE VÉRIFICATIONS ********
 
 -- Vérifier si une réaction est activée (c'est-à-dire tous les réactifs sont présents et aucun inhibiteur n'est présent)
--- verifReac ["a","c","d"] (Reaction ["a"] ["b"] ["c"])
 verifReac :: Sequence -> Reaction -> Bool
 verifReac sequence reaction =
     all (`elem` sequence) (reactifs reaction) && not (any (`elem` sequence) (inhibiteurs reaction))
 
 -- Test des séquences sur l'ensemble des réactions
--- verifSequence [["a"],["b"]] reactionTest1 --> [["c"],[]]
--- verifSequence [["a","c"],["b","c"],["a", ""],["b", ""]] reactionTest1 --> [["c"],["d"],["c"],[]]
 verifSequence :: [Sequence] -> [Reaction] -> [[Entites]]
 verifSequence sequences reactions =
     [ concat [produits reaction | reaction <- reactions, verifReac sequence reaction]
     | sequence <- sequences
     ]
 
--- verifSequenceEach [["a"],["b"]] reactionTest1 --> [["c"],[]]
--- verifSequenceEach [["a","c"],["b","c"],["a"],["b"]] reactionTest1 --> [["c","d"],["d"],["c"],[]]
 verifSequenceEach :: [Sequence] -> [Reaction] -> [[Entites]]
 verifSequenceEach sequences reactions =
-    [ applyReactionsUnique sequence reactions
-    | sequence <- sequences
-    ]
+    map (`appliquerReactionsUnique` reactions) sequences
 
--- procRec ["egf"] reactionTest2 --> [["erbb1","erk12"],["p70s6k"]]
 procRec :: Generateur -> [Reaction] -> [Sequence]
 procRec gLst reactions = processusRec [] gLst reactions
   where
     processusRec :: Sequence -> Generateur -> [Reaction] -> [Sequence]
     processusRec env gLst reactions =
         let initEnv = env ++ gLst
-         in notreNub (processusRecAux reactions initEnv)
+         in nub (processusRecAux reactions initEnv)
       where
         processusRecAux :: [Reaction] -> Sequence -> [Sequence]
         processusRecAux reactions env =
-            let newEnv = applyReactionsUnique env reactions
+            let newEnv = appliquerReactionsUnique env reactions
              in if null newEnv
                     then []
                     else newEnv : processusRecAux reactions newEnv
 
--- notreNub (cycle [1,2,3])
-notreNub :: (Eq a) => [a] -> [a]
-notreNub lst = reverse (notreNubAux lst [])
-  where
-    notreNubAux [] acc = acc
-    notreNubAux (x : xs) acc = if x `elem` acc then notreNubAux [] acc else notreNubAux xs (x : acc)
-
 -- Appliquer les réactions pour produire de nouvelles entités
--- applyReactionsUnique ["a","c"] reactionTest1 --> ["c","d"]
--- applyReactionsUnique ["b"] reactionTest1 --> []
-applyReactionsUnique :: Sequence -> [Reaction] -> Sequence
-applyReactionsUnique sequence reactions = foldl appliquerReactionsPourEntite [] sequence
+appliquerReactionsUnique :: Sequence -> [Reaction] -> Sequence
+appliquerReactionsUnique sequence reactions = foldl appliquerReactionsPourEntite [] sequence
   where
-    appliquerReactionsPourEntite acc entite = foldl ajouterUnique acc (concatMap produitsSiActive reactions) -- Appliquer les réactions pour une entité spécifique et mettre à jour la séquence finale
+    appliquerReactionsPourEntite acc entite = foldl ajouterUnique acc (concatMap produitsSiActive reactions)
       where
         produitsSiActive r = if verifReac [entite] r then produits r else []
-    ajouterUnique acc x = if x `elem` acc then acc else acc ++ [x] -- Ajouter des éléments uniques à la séquence résultante
+    ajouterUnique acc x = if x `elem` acc then acc else acc ++ [x]
 
--- doTree generateurTest reactionTest1 depth
-doTree :: Generateur -> [Reaction] -> Integer -> IO ()
-doTree generateur reactions depth = doTreeAux generateur reactions depth 0 [] [[]]
+genererTousCas :: Generateur -> [Reaction] -> Integer -> IO ()
+genererTousCas generateur reactions profondeur = genererTousCasAux generateur reactions profondeur 0 [[]]
   where
-    doTreeAux generateur reactions depth currentDepth currentRes previousRes =
-        Control.Monad.when (currentDepth < depth) $ do
+    genererTousCasAux generateur reactions profondeur currentDepth previousRes =
+        when (currentDepth < profondeur) $ do
             print previousRes
             let currentResTemp = verifSequenceEach previousRes reactions
-            let currentRes = concatMap (\res -> map (\g -> g : res) generateur) currentResTemp
-            doTreeAux generateur reactions depth (currentDepth + 1) [] currentRes
+            let currentRes = [g : res | res <- currentResTemp, g <- generateur]
+            genererTousCasAux generateur reactions profondeur (currentDepth + 1) currentRes
 
--- doTree generateurTest reactionTest1 depth
--- doTree :: Generateur -> [Reaction] -> Integer -> IO ()
--- doTree generateur reactions depth = doTreeAux generateur reactions depth 0 [] [[]]
---   where
---     doTreeAux generateur reactions depth currentDepth currentRes previousRes =
---         if currentDepth < depth
---             then do
---                 print previousRes
---                 let currentResTemp = verifSequenceEach previousRes reactions
---                 let currentRes = concatMap (\res -> map (\g -> g : res) generateur) currentResTemp
---                 doTreeAux generateur reactions depth (currentDepth + 1) [] currentRes
---             else return ()
+-- TESTS
+-- verifReac ["a","c","d"] (Reaction ["a"] ["b"] ["c"]) --> True
+-- verifSequence [["a"],["b"]] reactionTest1 --> [["c"],[]]
+-- verifSequence [["a","c"],["b","c"],["a", ""],["b", ""]] reactionTest1 --> [["c"],["d"],["c"],[]]
+-- verifSequenceEach [["a"],["b"]] reactionTest1 --> [["c"],[]]
+-- verifSequenceEach [["a","c"],["b","c"],["a"],["b"]] reactionTest1 --> [["c","d"],["d"],["c"],[]]
+-- procRec ["egf"] reactionTest2 --> [["erbb1","erk12"],["p70s6k"]]
+-- notreNub (cycle [1,2,3]) --> [1,2,3]
+-- appliquerReactionsUnique ["a","c"] reactionTest1 --> ["c","d"]
+-- appliquerReactionsUnique ["b"] reactionTest1 --> []
+-- genererTousCas generateurTest reactionTest1 profondeur
